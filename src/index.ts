bfeabe141fe5a518b8759e11186ec008f1bca2cc
Joona Piirainen <joona.piirainen@gmail.com>
2022-05-26 19:14:50 +0300

    Chore: Remove haskell refs

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,15 @@
 /**
  * I combinator - identity bird / idiot bird
- * `id` in Haskell
  */
 export const idiot: <T>(a: T) => T = (a) => a
 
 /**
  * K combinator - kestrel bird
- * `const` in Haskell
  */
 export const kestrel: <A, B>(a: A) => (b: B) => A = (a) => (_) => a
 
 /**
  * B combinator - cardinal bird
- * `flip` in Haskell
  */
 export const cardinal: <A, B, C>(
   f: (a: A) => (b: B) => C
@@ -20,14 +17,12 @@ export const cardinal: <A, B, C>(
 
 /**
  * A combinator - apply / applicator bird
- * `($)` in Haskell
  */
 export const applicator: <A, B>(f: (a: A) => B) => (a: A) => B = (f) => (a) =>
   f(a)
 
 /**
  * Psi combinator - psi bird (?)
- * `on` in Haskell
  */
 export const psi: <B, C>(
   f: (b: B) => (a: B) => C
