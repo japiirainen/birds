e796308478324f09214c395143e1cbe353f3b368
Joona Piirainen <joona.piirainen@gmail.com>
2022-05-26 15:01:06 +0300

    Feat: Add cardinal and applicator

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,26 @@
 /**
  * I combinator - identity bird / idiot bird
-* `id` in Haskell
+ * `id` in Haskell
  */
-export const idiot: <T>(a: T) => T = a => a
+export const idiot: <T>(a: T) => T = (a) => a
 
 /**
  * K combinator - kestrel bird
-* `const` in Haskell
+ * `const` in Haskell
  */
-export const kestrel: <A, B>(a: A) => (b: B) => A = a => _ => a
+export const kestrel: <A, B>(a: A) => (b: B) => A = (a) => (_) => a
+
+/**
+ * B combinator - cardinal bird
+ * `flip` in Haskell
+ */
+export const cardinal: <A, B, C>(
+  f: (a: A) => (b: B) => C
+) => (b: B) => (a: A) => C = (f) => (a) => (b) => f(b)(a)
+
+/**
+ * A combinator - apply / applicator bird
+ * `($)` in Haskell
+ */
+export const applicator: <A, B>(f: (a: A) => B) => (a: A) => B = (f) => (a) =>
+  f(a)
--- tests/index.ts
+++ tests/index.ts
@@ -1,4 +1,4 @@
-import { idiot, kestrel } from '../src'
+import { idiot, kestrel, cardinal, applicator } from '../src'
 import * as fc from 'fast-check'
 
 describe('birds', () => {
@@ -10,5 +10,21 @@ describe('birds', () => {
       fc.property(fc.anything(), fc.anything(), (a, b) =>
         expect(kestrel(a)(b)).toBe(a)
       )
+    ))
+
+  it('cardinal bird', () =>
+    fc.assert(
+      fc.property(fc.nat(), fc.nat(), (a, b) => {
+        const f = (a: number) => (b: number) => a + b
+        expect(cardinal(f)(a)(b)).toBe(f(b)(a))
+      })
+    ))
+
+  it('applicator bird', () =>
+    fc.assert(
+      fc.property(fc.nat(), (a) => {
+        const f = (b: number) => `${b}`
+        expect(applicator(f)(a)).toBe(f(a))
+      })
     ))
 })
